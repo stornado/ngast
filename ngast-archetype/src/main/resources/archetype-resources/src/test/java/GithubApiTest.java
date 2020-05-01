a60a3f9c3ae4e9bbc386e38743631d6f8301ca40
仲夏叶 <stornado@qq.com>
2020-05-01 20:19:41 +0800

    fix: remove unnecessary

--- ngast-archetype/src/main/resources/archetype-resources/src/test/java/GithubApiTest.java
+++ ngast-archetype/src/main/resources/archetype-resources/src/test/java/GithubApiTest.java
@@ -2,7 +2,6 @@ package ${package};
 
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.requestSpecification;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.everyItem;
 import static org.hamcrest.Matchers.greaterThan;
@@ -18,6 +17,7 @@ import io.qameta.allure.Story;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
@@ -36,6 +36,7 @@ public class GithubApiTest extends BaseNgastTestCase {
     @DataProvider
     public static Object[][] fetchRepositoryNames(Method method, ITestContext testCtx) {
         log.debug("{} {} {}", method.getName(), testCtx.getHost(), testCtx.getName());
+
         Object[][] objects = new Object[][] {
             { "Rest Assured", "stars" },
             { "TestNG", "stars" },
@@ -50,7 +51,11 @@ public class GithubApiTest extends BaseNgastTestCase {
         RestAssured.requestSpecification.baseUri("https://api.github.com");
     }
 
-    @Test(dataProvider = "fetchRepositoryNames", description = "搜索仓库地址", suiteName = "Github Api Test", testName = "Search Repositories", timeOut = 10_000)
+    @Test(dataProvider = "fetchRepositoryNames",
+        description = "搜索仓库地址",
+        suiteName = "Github Api Test",
+        testName = "Search Repositories",
+        timeOut = 10_000)
     @Story("示例搜索")
     @Description("参数化搜索仓库")
     @Step("搜索第一页数据")
@@ -79,13 +84,14 @@ public class GithubApiTest extends BaseNgastTestCase {
             .body("items", everyItem(hasKey("forks_count")))
             .body("items", everyItem(hasKey("watchers_count")));
 
+        JsonPath respJsonPath =  response.jsonPath();
         step("check first repo detail");
-        log.debug(response.jsonPath().getString("items[2].owner.login"));
+        log.debug(respJsonPath.getString("items[2].owner.login"));
 
         given()
             .get("/repos/{owner}/{repo}",
-                response.jsonPath().getString("items[2].owner.login"),
-                response.jsonPath().getString("items[2].name"))
+                respJsonPath.getString("items[2].owner.login"),
+                respJsonPath.getString("items[2].name"))
             .then()
             .statusCode(HttpStatus.SC_OK)
             .time(lessThanOrEqualTo(15L), TimeUnit.SECONDS)
@@ -93,10 +99,11 @@ public class GithubApiTest extends BaseNgastTestCase {
             .body("$", hasKey("stargazers_count"))
             .body("$", hasKey("forks_count"))
             .body("$", hasKey("watchers_count"))
-            .body("stargazers_count", equalTo(response.jsonPath().getInt("items[2].stargazers_count")))
-            .body("id", equalTo(response.jsonPath().getInt("items[2].id")))
-            .body("node_id", equalTo(response.jsonPath().getString("items[2].node_id")))
-            .body("full_name", equalTo(response.jsonPath().getString("items[2].full_name")));
+            .body("stargazers_count", equalTo(respJsonPath.getInt("items[2].stargazers_count")))
+            .body("id", equalTo(respJsonPath.getInt("items[2].id")))
+            .body("node_id", equalTo(respJsonPath.getString("items[2].node_id")))
+            .body("full_name", equalTo(respJsonPath.getString("items[2].full_name")));
 
     }
 }
+
